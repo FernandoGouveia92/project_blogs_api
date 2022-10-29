42c621ec39b5325d2b26eb14368bd86da68a6ef7
Fernando Gouveia <fernando_gouveia2@hotmail.com>
2022-10-29 15:50:52 -0300

    11 feito, 12 agora..

--- src/routers/post.router.js
+++ src/routers/post.router.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const postController = require('')
-const authMiddleware = require('../middlewares/auth.middleware');
+// const express = require('express');
+// const postController = require('')
+// const authMiddleware = require('../middlewares/auth.middleware');
 
-const router = express.Router();
+// const router = express.Router();
